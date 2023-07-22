1ee726baa75f6fc91b576e619fd16043aea0262e
Sirley1409 <sirleycristinaflorez@gmail.com>
2023-07-22 16:06:17 -0500

    creacion de api para la app

---
+++ HolaMundoAPI/HolaMundoAPi/Data/HolaMundoDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using HolaMundoAPi.Data.Models;
+
+namespace HolaMundoAPi.Data
+{
+    public class HolaMundoDbContext : DbContext
+    {
+        public HolaMundoDbContext (DbContextOptions<HolaMundoDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<HolaMundoAPi.Data.Models.Client> Clients { get; set; } = default!;
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Client>().ToTable(nameof(Client));
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ HolaMundoAPI/HolaMundoAPi/Data/SeedDb.cs
@@ -0,0 +1,38 @@
+﻿namespace HolaMundoAPi.Data
+{
+    public class SeedDb
+    {
+        private readonly HolaMundoDbContext context;
+        private readonly Random random;
+
+        public SeedDb(HolaMundoDbContext context)
+        {
+            this.context = context;
+            this.random = new Random();
+        }
+
+        public async Task SeedAsync()
+        {
+            await this.context.Database.EnsureCreatedAsync();
+
+            if (!this.context.Clients.Any())
+            {
+                this.AddUser("First Client");
+                this.AddUser("Second Client");
+                this.AddUser("Third Client");
+                await this.context.SaveChangesAsync();
+            }
+        }
+
+        private void AddUser(string name)
+        {
+            this.context.Clients.Add(new Models.Client
+            {
+                Name = name,
+                Dna = this.random.Next(1000000, 1999999).ToString()
+            });
+        }
+
+    }
+
+}
